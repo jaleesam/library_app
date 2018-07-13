c2d37df133816cb06f957e2f4bab2ce5d6620f80
Jaleesa McCrea <jmccrea01@gmail.com>
2018-07-12 23:16:54 -0400

    css and bootstrape code added

--- library/app.js
+++ library/app.js
@@ -5,16 +5,42 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3002;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootsrap/dist/js')));
 app.use('js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        title: 'Library'
+      });
+  });
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views', '/index.html'));
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
-app.listen(5000, () => {
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });

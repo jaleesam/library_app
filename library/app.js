8fe6e68d9787ccf5fada50256e354ebec80f7d8a
Jaleesa McCrea <jmccrea01@gmail.com>
2018-07-21 22:20:26 -0400

    added inital books

--- library/app.js
+++ library/app.js
@@ -18,13 +18,46 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+const books = [
+  {
+    title: 'Moonwalking with Einstein',
+    genre: 'nonfiction',
+    author: 'Joshua Foer',
+    read: false,
+  },
+  {
+    title: 'The Great Gasby',
+    genre: 'fiction',
+    author: 'F. Scott Fitzgerald',
+    read: true,
+  },
+  {
+    title: '1984',
+    genre: 'fiction',
+    author: 'George Orwell',
+    read: false,
+  },
+  {
+    title: 'How to Win Friends and Influence People',
+    genre: 'self-help',
+    author: 'Dale Carnegie',
+    read: true,
+  },
+  {
+    title: 'Salt Sugar Fat',
+    genre: 'health',
+    author: 'Michael Moss',
+    read: true,
+  }
+];
 bookRouter.route('/')
   .get((req, res) => {
     res.render('books',
       {
         nav: [{ link: '/books', title: 'Books' },
           { link: '/authors', title: 'Authors' }],
-        title: 'Library'
+        title: 'Library',
+        books
       });
   });
 bookRouter.route('/single')
